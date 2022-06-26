9a1b33ec3c518a7eed550dcf66f51b7fd9557a8d
jeremiahomueti <jeremiahomueti@gmail.com>
2022-06-26 20:02:41 +0100

    update

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const resultDisplay = document.getElementById('result')
 
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
+let computerChoice
+let result
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
 userChoice = e.target.id
 userChoiceDisplay.innerHTML = userChoice
 generateComputerChoice()
+getResult()
 }) ) 
 
 function generateComputerChoice() {
@@ -21,4 +24,45 @@ function generateComputerChoice() {
     if (randomNumber === 3) {computerChoice = 'Scissors'}
     
     computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() 
+{
+    if (computerChoice === userChoice)
+    {
+        result = "It's  a draw!"
+    }
+
+    if (computerChoice === 'rock' && userChoice === 'paper')
+    {
+        result = "You Win!"
+    }
+
+    if (computerChoice === 'rock' && userChoice === 'scissors')
+    {
+        result = "Oops! You Lose!"
+    }
+
+    if (computerChoice === 'paper' && userChoice === 'scissors')
+    {
+        result = "You Win!"
+    }
+    
+    if (computerChoice === 'paper' && userChoice === 'rock')
+    {
+        result = "Oops! You Lose!"
+    }
+    
+    if (computerChoice === 'scissors' && userChoice === 'rock')
+    {
+        result = "You Win!"
+    }
+
+    if (computerChoice === 'scissors' && userChoice === 'paper')
+    {
+        result = "Oops! You Lose!"
+    }
+    
+    resultDisplay.innerHTML = result
+
 }
